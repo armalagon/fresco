51128708d52746ab6c5ff6a02b753fb8d8b20c60
Armando Alaniz <arjoalaniz@gmail.com>
2018-08-13 00:56:18 -0600

    Usar bundle para manejo de internacionalizacion

---
+++ src/main/java/com/fresco/business/config/FrescoBundle.java
@@ -0,0 +1,28 @@
+package com.fresco.business.config;
+
+import com.zacate.i18n.BundleAggregator;
+import com.zacate.i18n.LocalizedConstants;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author Armando Alaniz
+ */
+public class FrescoBundle extends BundleAggregator {
+
+    public FrescoBundle() {
+        // TODO Add supported languages
+        super(Arrays.asList(Package.getPackages())
+                .stream()
+                .map(Package::getName)
+                .filter(name -> name.startsWith("com.fresco.business"))
+                .map(name -> name.substring(0, name.lastIndexOf(LocalizedConstants.DOT)))
+                .map(name -> new StringBuilder(name)
+                        .append(LocalizedConstants.DOT)
+                        .append(LocalizedConstants.I18N_PACKAGE_SUFFIX)
+                        .toString())
+                .collect(Collectors.toList()));
+    }
+
+}
