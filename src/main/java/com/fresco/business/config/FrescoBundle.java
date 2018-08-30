5f969ced7455f48542139c60730781e75a457303
Armando Alaniz <arjoalaniz@gmail.com>
2018-08-30 16:09:32 -0600

    Bundle personalizado para agrupar todos los bundles de la aplicacion en uno solo

--- src/main/java/com/fresco/business/config/FrescoBundle.java
+++ src/main/java/com/fresco/business/config/FrescoBundle.java
@@ -3,7 +3,9 @@ package com.fresco.business.config;
 import com.zacate.bean.Reflections;
 import com.zacate.i18n.BundleAggregator;
 import com.zacate.i18n.LocalizedConstants;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
@@ -14,8 +16,12 @@ public class FrescoBundle extends BundleAggregator {
 
     public FrescoBundle() {
         // TODO Add supported languages
-        super(Arrays.asList(Package.getPackages())
-                .stream()
+        super(findI18nPackages());
+    }
+
+    private static List<String> findI18nPackages() {
+        List<Package> basePackages = Arrays.asList(Package.getPackages());
+        List<String> backEndPackages = basePackages.stream()
                 .map(Package::getName)
                 .filter(name -> name.startsWith("com.fresco.business"))
                 .map(Reflections::getParentPackageName)
@@ -23,7 +29,15 @@ public class FrescoBundle extends BundleAggregator {
                         .append(LocalizedConstants.DOT)
                         .append(LocalizedConstants.I18N_PACKAGE_SUFFIX)
                         .toString())
-                .collect(Collectors.toList()));
+                .collect(Collectors.toList());
+        List<String> frontEndPackages = basePackages.stream()
+                .map(Package::getName)
+                .filter(name -> name.startsWith("com.fresco.presentation"))
+                .collect(Collectors.toList());
+        List<String> packages = new ArrayList<>(backEndPackages.size() + frontEndPackages.size());
+        packages.addAll(backEndPackages);
+        packages.addAll(frontEndPackages);
+        return packages;
     }
 
 }
